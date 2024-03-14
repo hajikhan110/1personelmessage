07ae32c54bef8607993122efa50717e34c7b7066
hajikhan <hajikhan110@yahoo.com>
2024-03-14 11:47:45 +0500

    msg

--- app.js
+++
@@ -1,9 +0,0 @@
-var personName = "";
-personName = prompt("what is your name please?") || "";
-if (personName !== null && personName !== "") {
-    alert(${personName},"would u like to learn some Python today?");
-}
-else {
-    alert("you have to submit your name");
-}
-console.log(personName);
